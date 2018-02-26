74e48b27a5e03b4da323e90dbb5c01e190e5df66
sandhiyakarthicsaran <35221190+sandhiyakarthicsaran@users.noreply.github.com>
2018-02-26 03:25:42 -0800

    range or not

---
+++ range.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number\n:",n);
+scanf("%d',n);
+if(n>=0&&n<=10)
+printf("YES..IT IS IN THE RANGE!!!\n");
+else
+printf("NO..IT IS NOT IN THE RANGE!!!\n");
+return 0;
+}
